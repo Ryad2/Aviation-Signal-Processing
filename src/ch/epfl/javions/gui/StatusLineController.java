a2a1a56cb9a991338ebf73b72b53a28d35a71c98
Ethan <borenethan@gmail.com>
2023-05-30 18:02:15 +0200

    Amélioration

--- src/ch/epfl/javions/gui/StatusLineController.java
+++ src/ch/epfl/javions/gui/StatusLineController.java
@@ -6,22 +6,29 @@ import javafx.beans.property.SimpleLongProperty;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
+/**
+ * La classe StatusLineController gère la ligne d'état.
+ * Elle possède un constructeur par défaut, qui construit le graphe de scène, ainsi que trois
+ * méthodes publiques.
+ *
+ * @author Ethan Boren (361582)
+ * @author Ryad Aouak (315258)
+ */
 public final class StatusLineController {
 
     private final BorderPane rootPane;
     private final LongProperty aircraftCountProperty = new SimpleLongProperty(0L);
     private final LongProperty messageCountProperty = new SimpleLongProperty(0L);
 
+
+    /**
+     * Constructeur de StatusLineController qui construit le graphe de scène
+     */
     public StatusLineController() {
 
-        Text aircraftCountText = new Text();
-        aircraftCountText.textProperty()
-                .bind(Bindings.format("Aéronefs visibles : %d", aircraftCountProperty));
+        Text aircraftCountText = textInStatusLine("Aéronefs visibles", aircraftCountProperty);
 
-        Text messageCountText = new Text();
-        messageCountText.textProperty()
-                .bind(Bindings.format("Messages reçus : %d", messageCountProperty));
-
+        Text messageCountText = textInStatusLine("Messages reçus", messageCountProperty);
 
         rootPane = new BorderPane(null, null,
                 messageCountText, null, aircraftCountText);
@@ -29,15 +36,37 @@ public final class StatusLineController {
         rootPane.getStylesheets().add("status.css");
     }
 
+    /**
+     * Retourne le panneau contenant la ligne d'état.
+     *
+     * @return le panneau contenant la ligne d'état.
+     */
     public BorderPane pane() {
         return rootPane;
     }
 
+    /**
+     * Retourne la propriété (modifiable) contenant le nombre d'aéronefs actuellement visibles.
+     *
+     * @return la propriété (modifiable) contenant le nombre d'aéronefs actuellement visibles.
+     */
     public LongProperty aircraftCountProperty() {
         return aircraftCountProperty;
     }
 
+    /**
+     * Retourne la propriété (modifiable) contenant le nombre de messages reçus depuis le début de
+     * l'exécution du programme.
+     *
+     * @return la propriété (modifiable) contenant le nombre de messages reçus.
+     */
     public LongProperty messageCountProperty() {
         return messageCountProperty;
+    }
+
+    private Text textInStatusLine (String indicatif, LongProperty property){
+        Text name = new Text();
+        name.textProperty().bind(Bindings.format("%s : %d", indicatif, property));
+        return name;
     }
 }
